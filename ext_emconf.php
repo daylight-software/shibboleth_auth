0aa0f18f5bde5338d4c5486bd16f65c9b06926f3
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2016-06-24 10:11:03 +0200

    [TASK] Add autoloading information to ext_emconf and composer.json, remove ext_autoload.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Shibboleth Single Sign On Authentication (BE & FE). The FE users will be imported automatically into the configured storage pid.',
 	'category' => 'services',
 	'shy' => 0,
-	'version' => '3.0.0',
+	'version' => '3.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -19,16 +19,16 @@ $EM_CONF[$_EXTKEY] = array(
 	'lockType' => '',
 	'author' => 'Tamer Erdogan / Lorenz Ulrich',
 	'author_email' => 'tamer.erdogan@univie.ac.at / lorenz.ulrich@visol.ch',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'author_company' => 'visol digitale Dienstleistungen GmbH',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-6.2.99',
+			'typo3' => '6.2.0-7.99.99',
 		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
+		'conflicts' => array(),
+		'suggests' => array(),
 	),
+	'autoload' =>
+		array(
+			'psr-4' => array('PHLU\\ShibbolethAuth\\' => 'Classes')
+		),
 );
